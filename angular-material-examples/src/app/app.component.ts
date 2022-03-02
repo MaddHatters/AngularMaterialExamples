a3260301f76866ea58d1212f3b79de50cc1b665f
Matt Beaulieu <30221259+MaddHatters@users.noreply.github.com>
2022-03-01 20:18:08 -0600

    Initial Create

---
+++ angular-material-examples/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, ViewChild } from '@angular/core';
+import { DateFnsAdapter, MAT_DATE_FNS_FORMATS } from '@angular/material-date-fns-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { enUS } from 'date-fns/locale';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  providers: [
+    { provide: DateAdapter, useClass: DateFnsAdapter },
+    //{ provide: MAT_DATE_LOCALE, useValue: 'enUS' }, // FYI, this currently fails for date-fns in angular material 13.2.4. Have to use the locale object from date-fns
+    { provide: MAT_DATE_LOCALE, useValue: enUS },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_DATE_FNS_FORMATS }
+  ]
+})
+export class AppComponent {
+  @ViewChild('picker')
+  public datepicker: any;
+
+
+  title = 'angular-material-examples';
+
+  constructor() {
+    // this.datepicker.setLocale(enUS)
+  }
+}
---
+++ angular-material-examples/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDateFnsModule } from '@angular/material-date-fns-adapter';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatDateFnsModule,
+    MatFormFieldModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
